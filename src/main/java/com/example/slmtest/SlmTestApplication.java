83c7916bd53f2b3ec78006f595b9efe051438576
David Zhou <wi21b026@technikum-wien.at>
2022-06-22 15:26:09 +0200

    Initial commit

---
+++ src/main/java/com/example/slmtest/SlmTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.slmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/slmtest/SlmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
